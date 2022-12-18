dee903b197d54ee48c1cf6a7511b4be298b86795
kenneth4832@gmail.com <kenneth4832@gmail.com>
2022-12-18 00:14:35 -0600

    Update: Dropdowns, new font for navbar

--- src/components/NavigationBar/NavigationBar.jsx
+++ src/components/NavigationBar/NavigationBar.jsx
@@ -3,7 +3,7 @@ import { useState } from 'react'
 import { Link } from 'react-router-dom'
 
 import { RxHamburgerMenu as OpenMenu } from "react-icons/rx"
-import { CgClose as CloseMenu} from 'react-icons/cg' 
+import { CgClose as HideMenu} from 'react-icons/cg' 
 
 const NavigationBar = () => {
   const [menuOpen, setMenuOpen] = useState(false)
@@ -11,28 +11,124 @@ const NavigationBar = () => {
   return (
     <div className="navBar">
       <div className="leftSection">
-        <a href='https://github.com/KennethOnuorah/Website' target={'_blank'} style={{textDecoration: "none"}}>
+        <a href='https://github.com/KennethOnuorah/Website' target={'_blank'} style={{textDecoration: "none", outline: "none"}}>
           <div className='companyLogo'>
-            <img src='../src/images/globe.png'/>
+            <img src='../src/images/globe_icon.png'/>
             <h1>Company</h1>
           </div>
         </a>
-        <div className="navDropdowns">
-          <h3>About Us</h3>
-          <h3>Services</h3>
-          <h3>Our Team</h3>
-          <h3>Blog</h3>
-          <h3>Contact Us</h3>
+          <div className="navBtns">
+            <div className="navDropdown">
+              <button>About Us</button>
+              <div className="aboutDropdownMenu">
+                <div className="menuItem">
+                  <h3>Who We Are</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                      Get to know more about our business and what we do to serve our fellow communities.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="menuItem">
+                  <h3>Our Mission</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                      Our mission at Company is to make a positive impact in the world through our work.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="menuItem">
+                  <h3>Meet the Team</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                      See all the amazing members of our team who help make our goals become reality.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="navDropdown">
+              <button>Services</button>
+              <div className="serviceDropdownMenu">
+                <div className="menuItem">
+                  <h3>Service #1</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="menuItem">
+                  <h3>Service #2</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="menuItem">
+                  <h3>Service #3</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+                <div className="menuItem">
+                  <h3>Service #4</h3>
+                  <hr/>
+                  <div className="dropdownDescription">
+                    <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                      <span>
+                        <a href='#' style={{textDecoration: "none"}}> Learn more.</a>
+                      </span>
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <button>Community</button>
+            <button>Blog</button>
+            <button>Contact Us</button>
+          </div>
+      </div>
+      <div className='rightSection'>
+        <div className="accountSection">
+          <button>Login</button>
+          <button>Sign Up</button>
         </div>
-      </div>
-      {/* We only want the .menuSection to appear and the .accountSection to disappear only when the screen width gets smaller */}
-      <div className="accountSection">
-        <button>Login</button>
-        <button>Sign Up</button>
-      </div>
-      <div className="menuSection">
-        {!menuOpen ? <OpenMenu className='menuBtn' onClick={() => setMenuOpen(true)}/>
-        : <CloseMenu className='closeBtn' onClick={() => setMenuOpen(false)}/>}
+        <div className="menuSection">
+          {
+            !menuOpen ? 
+              <OpenMenu className='menuBtn' onClick={() => setMenuOpen(true)}/> : 
+              <HideMenu className='hideBtn' onClick={() => setMenuOpen(false)}/>
+          }
+        </div>
       </div>
     </div>
   )
